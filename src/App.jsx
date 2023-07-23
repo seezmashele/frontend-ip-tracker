5a5df7edcb27e22eb86d23f2a151207b0aa60a44
seezmashele <seez.mash@hotmail.com>
2023-07-23 12:49:16 +0200

    checks if input is ip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,71 @@
 import "leaflet/dist/leaflet.css"
 import rightArrow from "/icon-arrow.svg"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import { useEffect, useState } from "react"
 
 function App() {
-  const position = [51.505, -0.09]
+  const [isp, setIsp] = useState("")
+  const [userIp, setUserIp] = useState("")
+  const [responseIp, setResponseIp] = useState("")
+  const [areaInfo, setAreaInfo] = useState("")
+  const [timezone, setTimezone] = useState("")
+  const [searchText, setSearchText] = useState("")
+  const [mapPosition, setMapPosition] = useState(null)
+  const myApiKey = "at_vWBG1GQaJ8cF7Bfw5I8FSDdnS2Klm"
+
+  const updateAllDetails = data => {
+    if (data && data.location) {
+      const loc = data.location
+      console.log("data - ", data)
+
+      setIsp(data.isp)
+      setResponseIp(data.ip)
+      setTimezone(loc.timezone)
+      setMapPosition([loc.lat, loc.lng])
+      setAreaInfo(
+        `${loc.city}, ${loc.country}${loc.postalCode && ", " + loc.postalCode}`
+      )
+    }
+  }
+
+  useEffect(() => {
+    fetch("https://api.ipify.org?format=json")
+      .then(response => response.json())
+      .then(data => setUserIp(data.ip))
+  })
+
+  useEffect(() => {
+    if (userIp) {
+      fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${myApiKey}&ipAddress=${userIp}`
+      )
+        .then(response => response.json())
+        .then(data => updateAllDetails(data))
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }, [userIp])
+
+  const checkValidIp = str => {
+    const regExp =
+      /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi
+
+    return regExp.test(str)
+  }
+
+  const handleAddressSearch = () => {
+    if (searchText) {
+      const requestString = checkValidIp(searchText)
+        ? `https://geo.ipify.org/api/v2/country,city?apiKey=${myApiKey}&ipAddress=${searchText}`
+        : `https://geo.ipify.org/api/v2/country,city?apiKey=${myApiKey}&domain=${searchText}`
+
+      fetch(requestString)
+        .then(response => response.json())
+        .then(data => updateAllDetails(data))
+        .catch(err => console.log(err))
+    }
+  }
 
   return (
     <>
@@ -15,17 +77,26 @@ function App() {
           <div className="w-full bg-white h-14f rounded-l-2xl overflow-hidden">
             <input
               type="text"
-              className="p-4 rounded-l-2xl w-full whitespace-nowrap text-[18px]"
+              value={searchText}
+              onChange={e => {
+                console.log(e.target.value)
+                setSearchText(e.target.value)
+              }}
+              className="p-4 rounded-l-2xl w-full whitespace-nowrap text-[18px] overflow-hidden text-ellipsis"
               placeholder="Search for any IP address or domain"
             />
           </div>
-          <div className="bg-black w-16 h-full cursor-pointer hover:bg-neutral-700 rounded-r-2xl flex items-center justify-center">
+          <button
+            type="button"
+            onClick={handleAddressSearch}
+            className="bg-black w-16 h-full cursor-pointer hover:bg-neutral-700 rounded-r-2xl flex items-center justify-center"
+          >
             <img
               src={rightArrow}
               alt=""
               className="w-3.5 h-3.5 object-contain"
             />
-          </div>
+          </button>
         </div>
       </div>
 
@@ -36,7 +107,7 @@ function App() {
               Ip address
             </h4>
             <p className="font-[500] text-2xl mt-2 overflow-hidden text-ellipsis very_dark_gray clap_lines">
-              192.120.473.542
+              {responseIp}
             </p>
           </div>
           <div className="w-full mt-7 md:mt-0 md:w-1/4 md:border-l md:pl-8 md:pr-5 responsive_text_center">
@@ -44,7 +115,7 @@ function App() {
               Location
             </h4>
             <p className="font-[500] text-2xl mt-2 very_dark_gray overflow-hidden text-ellipsis clap_lines">
-              Brooklyn, NY, 10001
+              {areaInfo}
             </p>
           </div>
           <div className="w-full md:w-1/4 mt-7 md:mt-0 md:border-l md:pl-8 md:pr-5 responsive_text_center">
@@ -52,7 +123,7 @@ function App() {
               Timezone
             </h4>
             <p className="font-[500] text-2xl mt-2 very_dark_gray overflow-hidden text-ellipsis clap_lines">
-              UTC - 5:00
+              {timezone && "UTC " + timezone}
             </p>
           </div>
           <div className="w-full md:w-1/4 mt-7 md:mt-0 md:border-l md:pl-8 responsive_text_center">
@@ -60,31 +131,31 @@ function App() {
               Isp
             </h4>
             <p className="font-[500] text-2xl mt-2 very_dark_gray overflow-hidden text-ellipsis clap_lines">
-              SpaceX
-              <br />
-              Starlink
+              {isp}
             </p>
           </div>
         </div>
       </div>
 
-      <div className="w-full h-[100vh] -mt-[5rem]f relative z-10 mt-[5rem]">
-        <MapContainer
-          className="w-full h-full"
-          center={[51.505, -0.09]}
-          zoom={14}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+      <div className="w-full h-[100vh] -mt-[5rem]f relative z-10 mt-[5rem] bg-[#ddd]">
+        {mapPosition && (
+          <MapContainer
+            className="w-full h-full"
+            center={mapPosition}
+            zoom={14}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={mapPosition}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        )}
       </div>
     </>
   )
